9104d51d26dde32e514e4c39877c07ca479ee6ed
MarcosQ88 <marcosquirque0@gmail.com>
2024-01-03 19:54:16 -0300

    importe bootstrap, axios, sweetaler2, el localhost mysql anda despues de un dia de joder

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,17 @@
 import './App.css';
+import { useState } from "react"; 
+//importacion de Axios. creo que es para manejar de forma mas facil los llamados al server
+import Axios from "axios";
+//importacion de bootstrap. libreria con botones, tablas y demas ya armado
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// importacion de sweetalert2. una libreria para tener notificiones ya prehechas
+// si no funca es por que tengo que instalar la version de react
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+//creo que esto seria como el objeto que se usa para invoccat a las notificacioness
+const noti = withReactContent(Swal);
 
 function App() {
   return (
